af6ee714396f9d7559ef8c7b8795a93da4db7980
Priscila <priscilasartori2@gmail.com>
2022-08-02 16:58:20 -0300

    Resolução do item 4

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    function quadro(){
+    let quadro = document.getElementById("pixel-board");
+    for(let index = 0; index <= 24; index += 1) {
+        let pixels = document.createElement("div");
+        pixels.className = "pixel";
+        quadro.appendChild(pixels);
+        }
+    } quadro();
+
+}

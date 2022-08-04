9f931880020c919de42a10971e404cf75ae70d16
Priscila <priscilasartori2@gmail.com>
2022-08-04 00:01:33 -0300

    Resolução do item 8

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-
 let preto = document.getElementById("preto");
 let amarelo = document.getElementById("amarelo");
 let azul = document.getElementById("azul");
 let vermelho = document.getElementById("vermelho");
+let colorSelect = "black";
 
-function selectionColor(event){
-    document.getElementById("preto").className = "color";
-    document.getElementById("amarelo").className = "color";
-    document.getElementById("azul").className = "color";
-    document.getElementById("vermelho").className = "color";
-    event.target.classList = "color selected";
-} 
-    preto.addEventListener("click", selectionColor);
-    amarelo.addEventListener("click", selectionColor);
-    azul.addEventListener("click", selectionColor);
-    vermelho.addEventListener("click", selectionColor);
-    
+preto.addEventListener("click", function(event){
+    amarelo.className = "color";
+    azul.className = "color";
+    vermelho.className = "color";
+    preto.classList = "color selected";
+    colorSelect = "black"
+});
+
+amarelo.addEventListener("click", function(event){
+    preto.className = "color";
+    amarelo.classList = "color selected";
+    azul.className = "color";
+    vermelho.className = "color";
+    colorSelect = "yellow"
+});
+
+azul.addEventListener("click", function(event){
+    amarelo.className = "color";
+    azul.className = "color selected";
+    vermelho.className = "color";
+    preto.classList = "color";
+    colorSelect = "blue"
+});
+
+vermelho.addEventListener("click", function(event){
+    amarelo.className = "color";
+    azul.className = "color";
+    vermelho.classList = "color selected";
+    preto.classList = "color";
+    colorSelect = "red"
+});
+
+function pixelboard(pixel){
+    pixel.style.backgroundColor = colorSelect;
+}
+console.log(colorSelect)

77f80fc305c153b002b906dd1be73661a00656c2
Priscila <priscilasartori2@gmail.com>
2022-08-05 23:40:03 -0300

    Resolução do item 10, 11 e 12

--- script.js
+++ script.js
@@ -1,43 +1,103 @@
 let preto = document.getElementById("preto");
-let amarelo = document.getElementById("amarelo");
-let azul = document.getElementById("azul");
-let vermelho = document.getElementById("vermelho");
-let pixel = document.getElementsByClassName('pixel')
+let cor1 = document.getElementById("cor1");
+let cor2 = document.getElementById("cor2");
+let cor3 = document.getElementById("cor3");
+let color1 = cor1.style.backgroundColor;
+let color2 = cor2.style.backgroundColor;
+let color3 = cor3.style.backgroundColor;
+let color = document.getElementsByClassName("color");
+let rgb;
+let pixelboard1 = document.getElementById("pixel-board");
+let pixel = document.getElementsByClassName('pixel');
 let colorSelect = "black";
-let clear = "white"
+let button = document.getElementById("generate-board");
+let size1 = document.getElementById("board-size");
+let value;
+
+function tabela(value){
+    pixelboard1.style.setProperty('--value', value);
+    for(let index = 0; index < value * value; index += 1) {
+    let div = document.createElement('div');
+    div.className = "pixel";
+    pixelboard1.appendChild(div)
+    }
+}
+
+button.addEventListener('click', function(event) {
+    event.preventDefault();
+    for (let index = pixelboard1.childNodes.length -1; index >= 0; index -= 1) {
+        const currentChildren = pixelboard1.childNodes[index];
+        currentChildren.remove();
+        }
+    let n = size1.value;
+     
+    if (n == "") {
+       alert('Board inválido!');
+    } else if (n < 5) {
+        value = 5;
+    } else if (n > 50) {
+        value = 50;
+    } else {
+        value = n;
+    }    
+    tabela(value); 
+})
+
+function geradorDeCor(){
+    const letras = "0123456789ABCDEF";
+    rgb = '#';
+    for (let index = 0; index < 6; index +=1){
+        rgb += letras.charAt(Math.floor(Math.random() * letras.length));
+    }
+    return rgb;
+}
+
+window.onload = function() {
+    cor1.style.backgroundColor = geradorDeCor();
+    cor2.style.backgroundColor = geradorDeCor();
+    cor3.style.backgroundColor = geradorDeCor();
+}
+console.log(cor1.style.backgroundColor)
+console.log(cor2.style.backgroundColor)
+console.log(cor3.style.backgroundColor)
 
 preto.addEventListener("click", function(event){
-    amarelo.className = "color";
-    azul.className = "color";
-    vermelho.className = "color";
+    cor1.className = "color";
+    cor2.className = "color";
+    cor3.className = "color";
     preto.classList = "color selected";
-    colorSelect = "black"
+    colorSelect = "black";
 });
 
-amarelo.addEventListener("click", function(event){
+cor1.addEventListener("click", function(event){
     preto.className = "color";
-    amarelo.classList = "color selected";
-    azul.className = "color";
-    vermelho.className = "color";
-    colorSelect = "yellow"
+    cor1.classList = "color selected";
+    cor2.className = "color";
+    cor3.className = "color";
+    colorSelect = color1;
 });
 
-azul.addEventListener("click", function(event){
-    amarelo.className = "color";
-    azul.className = "color selected";
-    vermelho.className = "color";
+cor2.addEventListener("click", function(event){
+    cor1.className = "color";
+    cor2.className = "color selected";
+    cor3.className = "color";
     preto.classList = "color";
-    colorSelect = "blue"
+    colorSelect = color2;
 });
 
-vermelho.addEventListener("click", function(event){
-    amarelo.className = "color";
-    azul.className = "color";
-    vermelho.classList = "color selected";
+cor3.addEventListener("click", function(event){
+    cor1.className = "color";
+    cor2.className = "color";
+    cor3.classList = "color selected";
     preto.classList = "color";
-    colorSelect = "red"
+    colorSelect = color3;
 });
 
-function pixelboard(pixel){
-    pixel.style.backgroundColor = colorSelect;
+function clickPalelle() {
+    for(let index = 0; index < pixel.length; index += 1) {
+        pixel[index].addEventListener("click", function(event) {
+            pixel[index].style.background = colorSelect;
+        })
+    }
 }
+clickPalelle()
